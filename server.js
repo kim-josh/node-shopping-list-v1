c1da3dec0ee5ca5add5762ba4d735e905d13da46
Josh Kim <josh94kim@berkeley.edu>
2017-08-31 13:35:22 -0700

    added Recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,23 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// this adds recipes to the list
+Recipes.create(
+  'chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create(
+  'chicken nuggets', ['bread crumbs', 'garlic powder', 'dried thyme', 'Dijon mustard', 'chicken tenders']);
+Recipes.create(
+  'carne asada tacos', ['tortilla', 'carne asada', 'cilantro', 'onions', 'tomatoes']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+// will return all current recipes
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
